55aba13a4f70de987708b77424474963d5cbaff7
SooYeol Baek <merrygoaround0726@gmail.com>
2022-09-06 23:57:32 +0900

    진행중 - MainActivity.kt - state, shared의 부연설명, shareIn과 state 예제 넣고, channel 예제넣고 마무리

--- flow3/src/main/java/com/example/flow3/MainActivity.kt
+++ flow3/src/main/java/com/example/flow3/MainActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.flow3.databinding.ActivityMainBinding
 import com.example.shared.model.uistate.UiState
 import kotlinx.coroutines.launch
@@ -20,15 +22,38 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         lifecycleScope.launch {
-            collecter()
+            /**
+             * flow는 livedata처럼 lifecycle의 흐름대로 자동으로 종료되거나 재시작 되지 않습니다.
+             * 만약 앱이 백그라운드로 가더라도 계속 emit된 값을 collect합니다.
+             * 그래서 repeatOnLifecycle Lifecycle.State.STARTED를 사용합니다.
+             * STARTED를 보게 되면,
+             * activity가 onstart 되는 시점이나 onPause 되는 시점에 새로운 coroutine으로 시작되며, onStop시점에 cancel 됩니다.
+             * */
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                //StateFlow
+                launch {
+                    vm.uiState.collect { state ->
+                        if (state is UiState.Success<*>) {
+                            Log.d("uiState : ", state.data.toString())
+                        }
+                    }
+                }
+                //SharedFlow
+                launch {
+                    vm.uiShared.collect { state ->
+                        if (state is UiState.Success<*>) {
+                            Log.d("uiShared : ", state.data.toString())
+                        }
+                    }
+                }
+            }
         }
+        clickListener()
     }
 
-    private suspend fun collecter() {
-        vm.uiState.collect { state ->
-            if (state is UiState.Success<*>) {
-                //recyclerview 만들기
-            }
+    private fun clickListener() {
+        binding.sameData.setOnClickListener {
+            vm.setSameData()
         }
     }
 }
--- flow3/src/main/java/com/example/flow3/MainViewModel.kt
+++ flow3/src/main/java/com/example/flow3/MainViewModel.kt
@@ -4,18 +4,43 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.flow3.resource.BlogFactory.createBlogList
 import com.example.shared.model.uistate.UiState
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
-    private val _uiState  = MutableStateFlow<UiState>(UiState.Loading)
-    val uiState : StateFlow<UiState> = _uiState.asStateFlow()
+    private val _uiState = MutableStateFlow<UiState>(UiState.Loading)
+    val uiState: StateFlow<UiState> = _uiState.asStateFlow()
+
+    //설정할 수 있다는 것만 보여주기 위함
+    //아래 설정은 다 기본값
+    private val _uiShared = MutableSharedFlow<UiState>(
+        replay = 0,
+        extraBufferCapacity = 0,
+        onBufferOverflow = BufferOverflow.SUSPEND
+    )
+
+
+    val uiShared: SharedFlow<UiState> = _uiShared.asSharedFlow()
 
     init {
         viewModelScope.launch {
             createBlogList().collect {
                 _uiState.value = UiState.Success(it)
+                //SharedFlow는 value가 없음
+                _uiShared.emit(UiState.Success(it))
+            }
+        }
+    }
+
+    //같은 데이터 넣기
+    fun setSameData() {
+        viewModelScope.launch {
+            createBlogList().collect {
+                _uiState.value = UiState.Success(it)
+                //SharedFlow는 value가 없음
+                _uiShared.emit(UiState.Success(it))
             }
         }
     }
