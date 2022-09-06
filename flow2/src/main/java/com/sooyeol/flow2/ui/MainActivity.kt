7717712fd0e894505d839bf9c7c6bc070890c78b
qjsqjsaos <merrygoaround0726@gmail.com>
2022-09-06 16:06:50 +0900

    flow2 완료

--- flow2/src/main/java/com/sooyeol/flow2/ui/MainActivity.kt
+++ flow2/src/main/java/com/sooyeol/flow2/ui/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.sooyeol.flow2.ui
 
-import android.os.Build.VERSION_CODES.M
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import com.example.shared.model.UiBlog
 import com.sooyeol.flow2.databinding.ActivityMainBinding
 import com.sooyeol.flow2.ui.uistate.UiState
 import kotlinx.coroutines.launch
@@ -37,7 +34,7 @@ class MainActivity : AppCompatActivity() {
                 if (state is UiState.Success<*>) {
                     Log.d("결과", state.data.toString())
                 } else {
-                    Log.d("결과2", (state as UiState.Failure).exception?.message!!.toString())
+                    Log.d("결과2", (state as UiState.Failure).toString())
                 }
             }
         }
--- flow2/src/main/java/com/sooyeol/flow2/ui/MainViewModel.kt
+++ flow2/src/main/java/com/sooyeol/flow2/ui/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.sooyeol.flow2.ui
 
+import android.accounts.NetworkErrorException
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -9,23 +10,45 @@ import com.sooyeol.flow2.ui.mapper.toUiBlogList
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
+/**
+ * 플로우의 수집은 정상적으로 완료되거나, 예외가 일어나서 완료되거나 해서
+ * 끝나게 된다.
+ * 오늘 사용해볼 onCompletion과 catch는 try catch finally와 매우 유사하다.
+ * 그렇다면 try catch와 무엇이 다를까
+ * onCompletion : onCompletion은 collect에서 flow의 수집을 모두 완료했을때 호출되며,
+ * catch에서 일어난 예외에 대한 값을 받을 수 있어서, 어떤 예외가 발생했는지 종료시점에 알 수 있다.
+ * catch : try catch에서 catch에 해당하는 부분이다. 에러사항을 Throwable 객체로 받을 수 있다.
+ * flow에서 NetworkErrorException() 예외가 일어났을때
+ * 실행되는 순서는 collect -> onCompletion -> catch 입니다.
+ * */
 class MainViewModel(private val useCase: UseCase) : ViewModel() {
 
     private val _blogList  = MutableStateFlow<UiState>(UiState.Loading)
     val blogList : StateFlow<UiState> = _blogList.asStateFlow()
 
+    private var isLast = false
+
     init {
         viewModelScope.launch {
             flow{
                 emit(useCase.getBlogList().toUiBlogList())
-            }.onCompletion {
-                Log.d("onCompletion", "Success")
+                throw NetworkErrorException()
+            }.onCompletion { e ->
+                Log.d("onCompletion", e.toString())
+                if(isLast) {
+                    Log.d("onCompletion", "isLast")
+                }
             }.catch { e ->
-                Log.d("catch Error", e.message.toString())
+                Log.d("catch Error", e.toString())
                 _blogList.emit(UiState.Failure(e))
             }.collect {
-                Log.d("collect Success", it.toString())
-                _blogList.emit(UiState.Success(it))
+                //서버에서 요청한 리스트가 빈값이라면,
+                if(it.isNotEmpty()) {
+                    Log.d("collect Success", it.toString())
+                    _blogList.emit(UiState.Success(it))
+                } else {
+                    isLast = true
+                }
             }
         }
     }
