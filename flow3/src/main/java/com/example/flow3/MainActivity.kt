fdf295655d3db2da1c9be11f71790a9427435e97
qjsqjsaos <merrygoaround0726@gmail.com>
2022-09-07 11:38:35 +0900

    flow3 완료 - channel 기능 추가

--- flow3/src/main/java/com/example/flow3/MainActivity.kt
+++ flow3/src/main/java/com/example/flow3/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.example.flow3.databinding.ActivityMainBinding
 import com.example.shared.model.uistate.UiState
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.flow.consumeAsFlow
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -70,14 +72,72 @@ class MainActivity : AppCompatActivity() {
                  * 하지만, SharedFlow는 conflate가 구현되어 있지않다.
                  * conflate() 메서드를 통해 conflate 기능을 따로 구현해주어야 한다.
                  * */
+
+                /**
+                 * 정리
+                 *
+                 * StateFlow 특징
+                 * 초깃값이 필요하다. 생성자에 초기값을 반드시 명시해야한다. (항상 값을 가지고 있고, 오직 한 가지 값을 가진다.)
+                 * emit()과 value 속성 이용가능
+                 * collector의 수에 관계없이 항상 구독하고 있는 것의 최신 값을 받는다.
+                 * hot stream이다.
+                 * collect를 활성화한 시점부터 방출된 데이터를 가져온다.
+                 * 업데이트 된 후에 값을 반환하고 동일한 값을 반환하지 않습니다.(conflate)
+                 *
+                 * SharedFlow 특징
+                 * 초깃값이 필요없습니다.
+                 * value 사용 불가, emit()만 사용가능
+                 * StateFlow의 기본적인 원형의 가까운 형태로 볼 수 있다.
+                 * StateFlow처럼 최신값을 보내는 것과는 다르게 내보낼 이전의 값 수를 구성할 수 있다. (replay)
+                 * emit된 데이터를 저장(cache)할 버퍼의 갯수를 지정할 수 있습니다. (extraBufferCapacity)
+                 * buffer가 다 찼을때 동작을 정의할 수 있습니다. (onBufferOverflow)
+                 * hot stream입니다.
+                 * 업데이트하면 이전 값이 동일하든 말든 값이 반환됩니다. (conflate 따로 설정해야함.)
+                * */
+
+                launch {
+                    vm.uiChannel
+//                        .consumeAsFlow() //find, map, filter, first 같은 함수들을 사용하려면 cosumeAsFlow()
+                    //함수를 이용해 flow로 전환하고 사용해야한다.
+
+//                        .receive() //리시브로 받는 방법
+                    //이 방법을 이용하려면 채널의 데이터를 보내지 않거나, 받지 않으면 close()메서드를
+                    //통해 채널을 종료해주어야한다. close() 메서드 이후 send()나 receive() 메서드를 호출하면,
+                    // ClosedReceiveChannelException 예외가 발생됩니다.
+
+//                        .consumeEach {  } //consumeEach와 아래 코드처럼 for문을 돌리는 방법은
+                    //실행하는데 매우 유사한 방법이지만,
+                    //consumeEach는 실행중 에러가 발생하면, 채널 자체가 close되지만,
+                    //for문을 돌리는 방법은 for문만 중단이 되고, channel은 종료되지 않는다.
+                    //이 채널을 사용하고 있는 다른 곳은 정상적인 동작을 이어 간다는 것이다.
+                    //가이드 문서에는 for-loop를 완벽하게 안전한 것이라 표현함.
+                    //데이터를 receive할때에는 for-loop사용 권장
+
+                    for(x in vm.uiChannel) {
+                        if (x is UiState.Success<*>) {
+                            Log.d("uiChannel : ", x.data.toString())
+                        }
+                    }
+                }
+
+                /**
+                 * Channel 특징
+                 * flow가 단일방향으로 데이터를 던지고 받는 형식이라면,
+                 * channel은 여러 방향에서 데이터를 던지고 받는 형식, 코루틴 끼리의 데이터를 전달해준다.
+                 * 현재 위 코드 사용은 클릭리스너를 위한 방법이므로, 일반적으로는 코루틴 간의 데이터 통신을 위해 사용한다.
+                 * 데이터 배출은 hot stream이어서 사용을 기다리지 않고, 한번에 배출한다.
+                 * 쉽게 말해 한번 밖에 배출하지 않아서, flow처럼 여러곳에서 동일한 데이터를 받는 것은 불가능하다.
+                 * 버퍼 사이즈나 형태를 지정할 수 있다.
+                 * FIFO (first-in first-out) 선입선출식으로 동작하며, 하나의 채널이 독점하지 않고,
+                 * 순차적으로 데이터를 가져간다.
+                 * */
             }
         }
         clickListener()
     }
 
     private fun clickListener() {
-        binding.sameData.setOnClickListener {
-            vm.setSameData()
-        }
+        //데이터 가져오기
+        binding.sameData.setOnClickListener { vm.getData() }
     }
 }
--- flow3/src/main/java/com/example/flow3/MainViewModel.kt
+++ flow3/src/main/java/com/example/flow3/MainViewModel.kt
@@ -5,14 +5,17 @@ import androidx.lifecycle.viewModelScope
 import com.example.flow3.resource.BlogFactory.createBlogList
 import com.example.shared.model.uistate.UiState
 import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
+    //StateFlow
     private val _uiState = MutableStateFlow<UiState>(UiState.Loading)
     val uiState: StateFlow<UiState> = _uiState.asStateFlow()
 
+    //SharedFlow
     //설정할 수 있다는 것만 보여주기 위함
     //아래 설정은 다 기본값
     private val _uiShared = MutableSharedFlow<UiState>(
@@ -20,27 +23,25 @@ class MainViewModel : ViewModel() {
         extraBufferCapacity = 0,
         onBufferOverflow = BufferOverflow.SUSPEND
     )
-
-
     val uiShared: SharedFlow<UiState> = _uiShared.asSharedFlow()
 
-    init {
-        viewModelScope.launch {
-            createBlogList().collect {
-                _uiState.value = UiState.Success(it)
-                //SharedFlow는 value가 없음
-                _uiShared.emit(UiState.Success(it))
-            }
-        }
-    }
 
-    //같은 데이터 넣기
-    fun setSameData() {
+    //Channel
+    //Channel.BUFFERED 버퍼 설정이 가능하다
+    //Channel.BUFFERED는 시스템이 정한 버퍼값 64개를 가지고 있는 상태로 설정한다는 뜻이다.
+    val uiChannel = Channel<UiState>(Channel.BUFFERED)
+
+    init { getData() }
+
+    fun getData() {
         viewModelScope.launch {
             createBlogList().collect {
-                _uiState.value = UiState.Success(it)
-                //SharedFlow는 value가 없음
-                _uiShared.emit(UiState.Success(it))
+                UiState.Success(it).let { value ->
+                    _uiState.value = value
+                    //SharedFlow는 value가 없음
+                    _uiShared.emit(value)
+                    uiChannel.send(value)
+                }
             }
         }
     }
